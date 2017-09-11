6b13f3cc2cdca7cf1497b45f439d9563e9763ad4
A1exeyDunin <oleksii.dunin@gmail.com>
2017-09-11 22:47:28 +0300

    initial project version

--- app/library/Total/Total.php
+++ app/library/Total/Total.php
@@ -9,8 +9,11 @@ class Total
 {
 
 
-
-
+    /**
+     * @param $id
+     * @return Buildings|Buildings[]|int|\Phalcon\Mvc\Model\ResultSetInterface
+     * Возвращает количество строений для той или иной улицы
+     */
     public function getTotal($id)
     {
         $total = Buildings::find("street_id = $id");
