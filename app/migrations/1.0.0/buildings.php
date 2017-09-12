d488a323611eaf7ef1b229033907ecadb5751930
A1exeyDunin <oleksii.dunin@gmail.com>
2017-09-12 11:00:09 +0300

    initial project version

---
+++ app/migrations/1.0.0/buildings.php
@@ -0,0 +1,84 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+/**
+ * Class BuildingsMigration_100
+ */
+class BuildingsMigration_100 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+        $this->morphTable('buildings', [
+                'columns' => [
+                    new Column(
+                        'id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'first' => true
+                        ]
+                    ),
+                    new Column(
+                        'street_id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'notNull' => true,
+                            'after' => 'id'
+                        ]
+                    ),
+                    new Column(
+                        'title',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 100,
+                            'after' => 'street_id'
+                        ]
+                    ),
+                    new Column(
+                        'number',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'notNull' => true,
+                            'after' => 'title'
+                        ]
+                    )
+                ],
+                'indexes' => [
+                    new Index('buildings_pkey', ['id'], null)
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+
+}
---
+++ app/migrations/1.0.0/streets.php
@@ -0,0 +1,68 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+/**
+ * Class StreetsMigration_100
+ */
+class StreetsMigration_100 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+        $this->morphTable('streets', [
+                'columns' => [
+                    new Column(
+                        'id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'first' => true
+                        ]
+                    ),
+                    new Column(
+                        'title',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 100,
+                            'after' => 'id'
+                        ]
+                    )
+                ],
+                'indexes' => [
+                    new Index('streets_pkey', ['id'], null)
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+
+}
